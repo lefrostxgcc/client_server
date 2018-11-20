9221128372adb1852c329293115643c1c19af9d2
Chip <chipz68q@gmail.com>
2018-11-20 20:11:43 +0500

    Add function create_server_socket
    Move server socket creating in separate
    function create_server_socket

--- src/server/main.c
+++ src/server/main.c
@@ -1,51 +1,27 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <arpa/inet.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+static int create_server_socket(int port, int backlog);
+
 int main(int argc, char *argv[]) 
 {
-	char buf[256] = {};
-	struct sockaddr_in channel;
-	int server_fd;
-	int client_socket;
-	int opt;
-	int count;
-	int buf_written_len;
+	char	buf[256] = {};
+	int		server_socket;
+	int		client_socket;
+	int		count;
+	int		buf_written_len;
 
-	opt = 1;
-	channel.sin_family = AF_INET;
-	channel.sin_addr.s_addr = htonl(INADDR_ANY);
-	channel.sin_port = htons(8000);
-
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-	{
-		perror("socket failed"); 
-		exit(EXIT_FAILURE); 
-	}
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-					&opt, sizeof(opt)) < 0)
-	{
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-	if (bind(server_fd, (struct sockaddr *) &channel, sizeof(channel)) < 0)
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
-	if (listen(server_fd, 10) < 0)
-	{
-		perror("listen");
-		exit(EXIT_FAILURE);
-	}
+	server_socket = create_server_socket(8000, 10);
 
 	count = 0;
 	while (1)
 	{
-		client_socket = accept(server_fd, 0, 0);
+		client_socket = accept(server_socket, 0, 0);
 		if (client_socket < 0)
 		{
 			perror("accept");
@@ -60,4 +36,68 @@ int main(int argc, char *argv[])
 	}
 
 	return 0; 
-} 
+}
+
+static int create_server_socket(int port, int backlog)
+{
+	struct sockaddr_in	channel;
+	int					server_socket;
+	int					opt;
+
+	opt = 1;
+	channel.sin_family = AF_INET;
+	channel.sin_addr.s_addr = htonl(INADDR_ANY);
+	channel.sin_port = htons(port);
+
+	if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	{
+		perror("socket failed"); 
+		exit(EXIT_FAILURE); 
+	}
+	if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+					&opt, sizeof(opt)) < 0)
+	{
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+	if (bind(server_socket, (struct sockaddr *) &channel, sizeof(channel)) < 0)
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	if (listen(server_socket, backlog) < 0)
+	{
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+
+	return server_socket;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
